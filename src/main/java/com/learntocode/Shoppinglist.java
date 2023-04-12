4fc1c6909f782b349fcf156000c698bc9f7c53cc
Vida Ulloa <lebeauarbre@gmail.com>
2023-04-12 11:38:38 -0400

    add a new shopping list class

---
+++ src/main/java/com/learntocode/Shoppinglist.java
@@ -0,0 +1,16 @@
+package com.learntocode;
+
+public class Shoppinglist {
+    public static void main(String[] args) {
+        System.out.println("oranges");
+        System.out.println("Apple");
+        System.out.println("Candy");
+        System.out.println("chips");
+        System.out.println("soda");
+        System.out.println("pasta");
+        System.out.println("milk");
+        System.out.println("juice");
+        System.out.println("yogurt");
+        System.out.println("tea");
+    }
+}
